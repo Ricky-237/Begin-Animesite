1e3052474aa3ae5b9fbdd0f86ca8391c7eae0d35
Ricky-237 <110074174+Ricky-237@users.noreply.github.com>
2025-02-16 13:02:00 +0100

    v

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ const script = `
 
 app.post('/execute-js', async (req, res) => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
         const page = await browser.newPage();
         await page.goto(url);
 
